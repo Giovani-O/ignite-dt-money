258cf9dabd4d9da5c1b3b5b5eb6b16570d3259dc
Giovani de Oliveira <giovani98@proton.me>
2024-02-09 14:15:31 -0300

    config: Installs Styled Components and theme configuration

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'primary-100': '#E1E1E6',
+  'primary-300': '#C4C4CC',
+  'primary-400': '#8D8D99',
+  'primary-500': '#7C7C8A',
+  'primary-600': '#323238',
+  'primary-700': '#29292E',
+  'primary-800': '#202024',
+  'primary-900': '#121214',
+
+  'secondary-300': '#a5b4fc',
+  'secondary-500': '#6366f1',
+  'secondary-700': '#4338ca',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'red-300': '#F75A68',
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+} as const
